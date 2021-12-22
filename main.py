7b40b5da7f39a232d547a81f8bcc2128f50c0637
Mounted Police <vladimir.spirin.bsu1801@yandex.ru>
2021-12-22 17:57:42 +0300

    Init

--- main.py
+++ main.py
@@ -11,17 +11,17 @@ SLEEP_TIME = 60
 
 # TCP SET
 TCP_IP = 'localhost'
-TCP_PORT = 8686
+TCP_PORT = 2549
 SERVER_ADRESS = (TCP_IP, TCP_PORT)
 BUFFER_SIZE = 1024
 LISTEN = 20
 
 # MySQL SET
-MYSQL_IP = 'localhost'
-MYSQL_PORT = "3306"
+MYSQL_IP = 'localhosy'
+MYSQL_PORT = "2417"
 DATABASE = 'MainCALCDB'
-MYSQL_USER = 'root'
-MYSQL_PASS = '12345678'
+MYSQL_USER = 'ssapp'
+MYSQL_PASS = 'pass'
 
 
 class server:
@@ -41,13 +41,14 @@ class server:
                 lprint("Connection established with message " + data)
                 data = data.split('-')
                 if data[0] == 'LICENCE':
+                    lprint('LICENCE')
                     threading.Thread(target=self._licenser, args=(client_socket, data[1], data[2])).start()
                 elif data[0] == 'AUTH':
                     lprint('AUTH')
-                    client_socket.close()
+                    threading.Thread(target=self._auth, args=(client_socket, data[1], data[2])).start()
                 elif data[0] == 'REG':
                     lprint('REG')
-                    client_socket.close()
+                    threading.Thread(target=self._register, args=(client_socket, data[1], data[2])).start()
                 else:
                     lprint('ошибка запроса')
                     self._send(client_socket, 'NO')
@@ -61,11 +62,11 @@ class server:
         try:
             self._send(client_socket, 'OK')
             while pony:
-                data = client_socket.recv(BUFFER_SIZE)
-                if not data:
-                    pass
-                else:
-                    lprint('Data : ' + str(data.decode("UTF-8")) + "\n")
+                    data = client_socket.recv(BUFFER_SIZE)
+                    if not data:
+                        pass
+                    else:
+                        lprint('Data : ' + str(data.decode("UTF-8")) + "\n")
         except socket.error:
             lprint('Client disconnected')
             try:
@@ -90,12 +91,30 @@ class server:
                 except:
                     lprint('ALL BAD CALL MOUNTED POLICE NOW')
 
-    def _licenser(self, socket, user, app):
+    def _licenser(self, sock, user, app):
         if self.db.get_licence(user, app):
-            self.CLIENTS.append(socket)
-            threading.Thread(target=self.client_handler, args=(socket,)).start()
+            self.CLIENTS.append(sock)
+            threading.Thread(target=self.client_handler, args=(sock,)).start()
         else:
-            self._send(socket, 'NO')
+            self._send(sock, 'NO')
+        sys.exit()
+
+    def _register(self, sock, user, password):
+        if self.db.hasUser(user):
+            self._send(sock, "REGNO")
+        else:
+            self.db.reg_user(user, password)
+            self._send(sock, "REGOK")
+        sock.close()
+        sys.exit()
+
+    def _auth(self, sock, user, password):
+        authed, permissions = self.db.get_auth(user, password)
+        if authed:
+            self._send(sock, permissions)
+        else:
+            self._send(sock, "NOAUTH")
+        sock.close()
         sys.exit()
 
     def _send(self, sock, message):
@@ -114,6 +133,19 @@ class dbClient:
         self.user = user
         self.password = password
 
+    def get_auth(self, username, password):
+        lprint("Auth " + username + " with pass " + password)
+        tab = self._select_fetchall('User')
+        for i in tab:
+            if i[1] == username and i[2] == password:
+                if i[3] == None:
+                    lprint(username + " ez Auth " + username + "with pass" + password + " permissions: 0")
+                    return [True, '']
+                else:
+                    lprint(username + " ez Auth " + username + "with pass" + password + " permissions: " + i[3])
+                    return [True, i[3]]
+        return False, False
+
     def get_licence(self, username, app):
         lprint(username + " Checking the user license for application " + app)
         tab = self._select_fetchall('User')
@@ -124,6 +156,52 @@ class dbClient:
                 lprint("License ok")
                 return True
         return False
+
+    def reg_user(self, username, password):
+        try:
+            conn = mysql.connector.connect(host=self.host,
+                                           port=self.port,
+                                           database=self.database,
+                                           user=self.user,
+                                           password=self.password)
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO `MainCALCDB`.`User` (`Username`, `Password`) VALUES ('"
+                           + username + "', '" + password + "');")
+            conn.commit()
+            cursor.close()
+            conn.disconnect()
+        except mysql.connector.errors.InterfaceError:
+            lprint('DB connection error')
+            return []
+        except:
+            lprint('DB unexpected error')
+            return []
+
+    def hasUser(self, username):
+        for i in self._get_users():
+            if i[0] == username:
+                return True
+        return False
+
+    def _get_users(self):
+        try:
+            conn = mysql.connector.connect(host=self.host,
+                                           port=self.port,
+                                           database=self.database,
+                                           user=self.user,
+                                           password=self.password)
+            cursor = conn.cursor()
+            cursor.execute("SELECT Username FROM " + self.database + ".User")
+            tab = cursor.fetchall()
+            cursor.close()
+            conn.disconnect()
+            return tab
+        except mysql.connector.errors.InterfaceError:
+            lprint('DB connection error')
+            return []
+        except:
+            lprint('DB unexpected error')
+            return []
 
     def _select_fetchall(self, table):
         try:
@@ -160,3 +238,4 @@ if __name__ == '__main__':
             time.sleep(SLEEP_TIME)
         except:
             sys.exit()
+
